78f21e751d108cd7a0db07c5491cba7002706467
mariopejic1 <mariopejic02@gmail.com>
2024-08-19 23:07:20 +0200

    login, registracija

--- webshop/views.py
+++ webshop/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 def base_view(request):
-    return render(request, 'webshop/base.html')
+    context = {}
+    return render(request, 'webshop/base.html', context)
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('base.html') 
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'webshop/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('base') 
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'webshop/login.html', {'form': form})
+
+def logout_view(request):
+    from django.contrib.auth import logout
+    logout(request)
+    return redirect('base.html')  
+
+def profile_view(request):
+    return render(request, 'webshop/profile.html')
--- zavrsni_rad/urls.py
+++ zavrsni_rad/urls.py
@@ -19,11 +19,12 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from webshop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("webshop/", include("webshop.urls")),
+    path('', views.base_view, name='base'),
     
 ]
 
